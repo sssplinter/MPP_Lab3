4ff01899dd0ba412c59c492d850f6bca7b9eabdf
Kristina <kristinasementsova21@gmaol.com>
2021-09-22 22:50:14 +0300

    implemented the main logic of the class mutex

--- Lab3/Mutex.cs
+++ Lab3/Mutex.cs
@@ -1,7 +1,26 @@
-﻿namespace Lab3
+﻿
+using System;
+using System.Threading;
+
+namespace Lab3
 {
     public class Mutex
     {
+        private int _lockThreadId;
         
+        public void Lock()
+        {
+            var currentThreadId = Thread.CurrentThread.ManagedThreadId;
+            while (Interlocked.CompareExchange(ref _lockThreadId, currentThreadId, 0) != 0)
+            {
+                Thread.Sleep(100);
+            }
+        }
+
+        public void Unlock()
+        {
+            var currentThreadId = Thread.CurrentThread.ManagedThreadId;
+            Interlocked.CompareExchange(ref _lockThreadId, 0, currentThreadId);
+        }
     }
 }
--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -1,9 +1,31 @@
-﻿namespace Lab3
+﻿using System;
+using System.Threading;
+
+namespace Lab3
 {
     internal class Program
     {
+        private static Mutex mutex = new Mutex();
+        
         public static void Main(string[] args)
         {
+            var thread1 = new Thread(Calculate);
+            thread1.Start();
+            var thread2 = new Thread(Calculate);
+            thread2.Start();
+            var thread3 = new Thread(Calculate);
+            thread3.Start();
+        }
+
+        private static void Calculate()
+        {
+            mutex.Lock();
+            for (var i = 0; i < 10; i++)
+            {
+                Console.Out.WriteLine("Thread {0} - {1}", Thread.CurrentThread.ManagedThreadId, i);
+                Thread.Sleep(500);
+            }
+            mutex.Unlock();
         }
     }
 }
